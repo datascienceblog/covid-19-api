8546849fe292dd465edd8c936b9991e5805874f7
Dusan Ku <d.ku@auckland.ac.nz>
2020-04-02 22:44:13 +0900

    Initial

--- test.py
+++ test.py
@@ -4,9 +4,10 @@ import pandas as pd
 
 # POST to API
 payload = {'country': 'Germany'}
-URL = 'https://api.statworx.com/covid'
+URL = 'http://127.0.0.1/covid'
+print('payload:', json.dumps(payload))
 response = requests.post(url=URL, data=json.dumps(payload))
-
+print('response:', response.text)
 # Convert to data frame
 df = pd.DataFrame.from_dict(json.loads(response.text))
 df.head()
